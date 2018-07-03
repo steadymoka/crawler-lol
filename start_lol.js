80dfcba69dae794a507f43db9512df76acbc921d
moka-a <aud1220a@gmail.com>
2018-07-03 23:07:24 +0900

    wip

--- start_lol.js
+++ start_lol.js
@@ -4,88 +4,9 @@ const {
 } = require("jsdom")
 const fs = require("fs")
 
-const URL_API_PREFIX = "https://api.github.com/repos/we-are-dodo/works/contents"
-const URL_GITHUB_PREFIX = "https://github.com/we-are-dodo/works/blob/master"
-
-async function getMarkdown(slug) {
-    const response = await axios.get(`${URL_GITHUB_PREFIX}/${slug}`)
-    const $dom = new JSDOM(response.data)
-    const $article = $dom.window.document.querySelector("article.markdown-body.entry-content")
-
-    const header = {}
-
-    const $table = $article.querySelector("table[data-table-type=yaml-metadata]")
-    console.log($table)
-    if ($table) {
-        const $tableHeaders = $table.querySelectorAll("thead th")
-        const $tableBodies = $table.querySelectorAll("tbody td")
-        for (const index of Object.keys($tableHeaders)) {
-            const $tableHeader = $tableHeaders[index]
-            const $tableBody = $tableBodies[index]
-            const $tableBodyCells = $tableBody.querySelectorAll("td")
-            if ($tableBodyCells.length) {
-                header[$tableHeader.textContent] = [...$tableBodyCells].map($cell => $cell.textContent)
-            } else {
-                header[$tableHeader.textContent] = $tableBody.textContent
-            }
-        }
-        $article.removeChild($table)
-    }
-    return {
-        header,
-        $body: $article,
-    }
-}
-
-async function getUser(date, username) {
-
-    const doc = await getMarkdown(`${date}/${username}.md`)
-    const user = {
-        id: username,
-        name: doc.header.name || null,
-        works: doc.header.works || [],
-        body: null,
-    }
-
-    if (doc.$body.innerHTML.trim()) {
-        const $headlines = doc.$body.querySelectorAll("h2")
-        user.works = [...$headlines].map($headline => $headline.textContent)
-        user.body = doc.$body.innerHTML.trim()
-    }
-    return user
-}
-
-async function getMeet(date) {
-    const doc = await getMarkdown(`${date}/readme.md`)
-    const response = await axios.get(`${URL_API_PREFIX}/${date}`)
-    const usernames = response.data
-        .filter(item => item.type === "file" && item.name !== "readme.md" && /\.md$/.test(item.name))
-        .map(item => item.name.replace(/\.md$/, ""))
-
-    return {
-        date,
-        contents: doc.$body.innerHTML.trim(),
-        users: await Promise.all(usernames.map(username => getUser(date, username))),
-    }
-}
-
-async function getMeets() {
-    const response = await axios.get(URL_API_PREFIX)
-    const dates = response.data
-        .filter(item => item.type === "dir" && /^20\d{2}-\d{2}-\d{2}/.test(item.name))
-        .map(item => item.name)
-
-    return {
-        meets: await Promise.all(dates.map(date => getMeet(date)))
-    }
-}
-
-/**
- * 
- */
-
 async function getLol() {
     const response = await axios.get("http://www.leagueoflegends.co.kr/?m=esports&mod=chams_schedule&cate=1")
+
     const $dom = new JSDOM(response.data)
     const $article = $dom.window.document.querySelector("div.esports-table.team-table")
 
@@ -95,21 +16,70 @@ async function getLol() {
 
     if ($table) {
         const $tableTR = $table.querySelectorAll("tbody tr");
-        
+
         for (const index of Object.keys($tableTR)) {
             var item = $tableTR[index];
-            console.log(item);
+
+            /* 경기 날짜 */
+            var matchDate;
+            var matchDateElement = item.querySelector("th");
+            if (matchDateElement)
+                matchDate = matchDateElement.innerHTML;
+
+            /* 경기 시간 */
+            var matchTime;
+            matchTime = item.getElementsByClassName("table-data__time")[0].innerHTML;
+
+            var score;
+            var scoreElement = item.getElementsByClassName("match match_count")[0];
+            if (scoreElement)
+                score = scoreElement.innerHTML.trim();
+            else
+                score = undefined;
+
+            /* 홈 팀 / 스코어 */
+            var homeTeam;
+            homeTeam = item
+                .getElementsByClassName("table-data__position-left")[0]
+                .getElementsByClassName("table-data__team-name")[0]
+                .innerHTML;
+
+            /* 어웨이 팀 / 스코어 */
+            var awayTeam;
+            awayTeam = item
+                .getElementsByClassName("table-data__position-right")[0]
+                .getElementsByClassName("table-data__team-name")[0]
+                .innerHTML;
+
+            /* 이긴팀 */
+            var winner;
+            var homeScore;
+            var awayScore;
+            if (score) {
+                homeScore = parseInt(score.split(":")[0]);
+                awayScore = parseInt(score.split(":")[1]);
+
+                winner = homeScore > awayScore ? "home" : "away";
+
+            } else {
+                homeScore = 0;
+                awayScore = 0
+
+                winner = undefined;
+            }
 
             const match = {
-                matchStartAt: 0,
+                matchDate: matchDate,
+                matchTime: matchTime,
                 home: {
-                    teamName: ""
+                    teamName: homeTeam.trim()
                 },
                 away: {
-                    teamName: ""
+                    teamName: awayTeam.trim()
                 },
-                home_score: 0,
-                away_score: 0
+                home_score: homeScore,
+                away_score: awayScore,
+                winner: winner
             }
             matchs.push(match)
         }
